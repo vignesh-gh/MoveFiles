d71adf49331c9a2667cd695843414d853dbd94f4
vigneshp <vigneshp@ELPBLR-049.evolvus.com>
2020-03-11 17:18:35 +0530

    Initial commit

---
+++ MoveF/WebContent/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="fileUpload" method="post" enctype="multipart/form-data">
+	<input type="file" name="file" multiple></br></br>
+	<input type="submit">Upload</input> 
+</form>
+
+</body>
+</html>
---
+++ MoveF/src/com/vignesh/MoveFiles/FileUpload.java
@@ -0,0 +1,44 @@
+package com.vignesh.MoveFiles;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.jni.Directory;
+
+
+
+public class FileUpload extends HttpServlet {
+	private static final long serialVersionUID = 1L;       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ServletFileUpload sr = new ServletFileUpload(new DiskFileItemFactory());
+		try {
+			
+			List<FileItem> mulFiles=sr.parseRequest(request);
+			
+			
+			for(FileItem item:mulFiles) {
+				item.write(new File("/home/vigneshp/"+item.getName()));
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
